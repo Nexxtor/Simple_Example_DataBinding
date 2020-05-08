de147066c9dd08b72b322aa6c1bebca167e1ca3f
Nestor <nestor.aldana1@gmail.com>
2020-05-08 08:53:26 -0600

    Add Complete Solution as Simple Databindig

--- app/src/main/java/com/naldana/databinding/MainActivity.kt
+++ app/src/main/java/com/naldana/databinding/MainActivity.kt
@@ -2,15 +2,37 @@ package com.naldana.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+
+import com.naldana.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     var inCounter = Counter("In", 0)
     var outCounter = Counter("Out", 0)
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Restablecer valores previos
+
+        binding.apply {
+            inCounter = this@MainActivity.inCounter
+            outCounter = this@MainActivity.outCounter
+
+            buttonAddIn.setOnClickListener {
+                this@MainActivity.inCounter.count++
+                invalidateAll()
+            }
+
+            buttonAddOut.setOnClickListener {
+                this@MainActivity.outCounter.count++
+                invalidateAll()
+            }
+        }
     }
 
 
